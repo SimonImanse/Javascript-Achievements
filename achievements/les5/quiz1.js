f2ea5007c0e88fe94148112660feba5e676e38c4
SimonImanse <30230@ma-web.nl>
2021-02-03 14:25:07 +0100

    opdracht quiz les 5 klaar

---
+++ achievements/les5/quiz1.js
@@ -0,0 +1,61 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "https://images3.persgroep.net/rcs/tEq_KObRoUyNsR3VxHAMMSaoQwY/diocontent/139016913/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Waar gaan de kunstwerken over in het museum?",
+				"answers": [{
+						"answer": "Motoren en Auto's",
+						"feedback": false
+					},
+					{
+						"answer": "Voelsel en Consumptie",
+						"feedback": true
+					},
+					{
+						"answer": "Dieren en Amfibieën ",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wanneer is het museum geopend?",
+				"answers": [{
+						"answer": "augustus 2003",
+						"feedback": false
+					},
+					{
+						"answer": "november 2010",
+						"feedback": false
+					},
+					{
+						"answer": "februari 2019",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "Wie is de architect van het museum?",
+				"answers": [{
+						"answer": "Sjoerd Soeters",
+						"feedback": false
+					},
+					{
+						"answer": "Rem Koolhaas",
+						"feedback": false
+					},
+					{
+						"answer": "Arie Korbee ",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
